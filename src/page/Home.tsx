41b4c9fe8b1336be30c02750239d2f4492a83f45
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-22 20:29:40 +0600

    added product interface and pass data to the productCard component

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,5 +1,6 @@
 import ProductsCard from "@/components/ProductsCard";
 import { useGetProductsQuery } from "@/redux/features/api/apiSlice";
+import { IProduct } from "@/types/globalTypes";
 
 const Home = () => {
   const { data, isLoading, error } = useGetProductsQuery(undefined);
@@ -7,10 +8,23 @@ const Home = () => {
   console.log(isLoading);
   console.log(error);
 
+  // interface IProduct {
+  //   name: string;
+  //   genre: string;
+  //   photo: string;
+  //   seller_name: string;
+  //   seller_email: string;
+  //   price: number;
+  //   description: string;
+  //   reviews: string[];
+  // }
+
   return (
     <div>
       <h1>Home</h1>
-      <ProductsCard />
+      {data?.map((products: IProduct) => (
+        <ProductsCard key={products?._id} products={products} />
+      ))}
     </div>
   );
 };
--- src/types/globalTypes.ts
+++ src/types/globalTypes.ts
@@ -8,3 +8,15 @@ export interface ILogin {
   email: string;
   password: string;
 }
+
+export interface IProduct {
+  _id: string;
+  name: string;
+  genre: string;
+  photo: string;
+  seller_name: string;
+  seller_email: string;
+  price: number;
+  description: string;
+  reviews: string[];
+}
