d69b97f651772d106671ac673b89fb94a36b924f
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-22 15:00:23 +0600

    use the useGetProductsQuery hook for receiving data

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,4 +1,11 @@
+import { useGetProductsQuery } from "@/redux/features/api/apiSlice";
+
 const Home = () => {
+  const { data, isLoading, error } = useGetProductsQuery(undefined);
+  console.log(data);
+  console.log(isLoading);
+  console.log(error);
+
   return (
     <div>
       <h1>Home</h1>
