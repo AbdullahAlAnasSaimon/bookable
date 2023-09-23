d6dd525464f6cbe1291c0f6cd331828c58932c16
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-23 15:41:25 +0600

    added product reducer to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
 import { productApi } from "./features/api/apiSlice";
+import productReducer from "./features/product/productSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    product: productReducer,
     [productApi.reducerPath]: productApi.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
