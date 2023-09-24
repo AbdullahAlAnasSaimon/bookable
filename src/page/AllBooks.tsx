d4d17536ee4afb342abb48f988e7e5ac8287d516
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-24 20:16:59 +0600

    added button for add new book

--- src/page/AllBooks.tsx
+++ src/page/AllBooks.tsx
@@ -1,6 +1,7 @@
 import Loader from "@/components/Loader";
 import ProductsCard from "@/components/ProductsCard";
 import SearchBar from "@/components/SearchBar";
+import { Button } from "@/components/ui/button";
 import { useGetProductsQuery } from "@/redux/features/api/apiSlice";
 import { IProduct } from "@/types/globalTypes";
 
@@ -14,6 +15,7 @@ const AllBooks = () => {
 
   return (
     <section className="w-11/12 mx-auto my-10">
+      <Button>Add New</Button>
       <SearchBar />
       <div className="grid grid-cols-4 gap-5">
         {data?.map((products: IProduct) => (
