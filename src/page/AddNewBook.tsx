a4a9977f4c460431aaea5fc02fa93c7db345e524
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-24 20:26:45 +0600

    created form for adding new book

--- src/page/AddNewBook.tsx
+++ src/page/AddNewBook.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
 const AddNewBook = () => {
   return (
     <div>
-      <h1>Add New Book</h1>
+      <h1>Add A New Book</h1>
+      <form>
+        <Input type="text" placeholder="Title" />
+        <Input type="text" placeholder="Genre" />
+        <Input type="text" placeholder="Price" />
+        <Input type="text" placeholder="Publication Date" />
+        <Textarea placeholder="Book Description" />
+        <Button variant="default">Add</Button>
+      </form>
     </div>
   );
 };
