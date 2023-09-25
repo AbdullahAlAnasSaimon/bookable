d2996fde9f804ce1dac3769bb8811a0f3807cc68
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-25 21:15:12 +0600

    added react router link in the book card and title

--- src/components/ProductsCard.tsx
+++ src/components/ProductsCard.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Card } from "./ui/card";
 import { IProduct } from "@/types/globalTypes";
+import { Link } from "react-router-dom";
 
 const ProductsCard = ({ product }: { product: IProduct }) => {
-  const { name, photo, genre, price, seller_name } = product;
+  const { _id, name, photo, genre, price, seller_name } = product;
 
   return (
     <>
       <Card>
-        <img
-          className="w-11/12 mx-auto h-[320px] mt-3 rounded-md"
-          src={photo}
-          alt={name}
-        />
+        <Link to={`/book/${_id}`}>
+          <img
+            className="w-11/12 mx-auto h-[320px] mt-3 rounded-md"
+            src={photo}
+            alt={name}
+          />
+        </Link>
         <div className="p-3">
-          <h2 className="my-2 text-lg font-semibold">{name}</h2>
+          <Link to={`/book/${_id}`}>
+            <h2 className="my-2 text-lg font-semibold">{name}</h2>
+          </Link>
           <p className="text-sm">Author: {seller_name}</p>
           <p className="my-1 text-sm">{genre}</p>
           <p className="mb-2 text-lg text-slate-900 font-bold">${price}</p>
