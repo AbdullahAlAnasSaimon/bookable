e7d8f7c5920bad20834b785e99a3a241e1f25794
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-25 22:25:31 +0600

    trying to show the book details data

--- src/page/BookDetails.tsx
+++ src/page/BookDetails.tsx
@@ -1,6 +1,12 @@
+import { useGetProductsQuery } from "@/redux/features/api/apiSlice";
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const BookDetails = () => {
+  const { data } = useGetProductsQuery(undefined);
+  const product = useParams();
+  console.log(product);
+  console.log(data);
   return <div></div>;
 };
 
