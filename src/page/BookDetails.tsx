4ef77911fc0769c2c4109d791fac87b12e16d2e2
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-26 21:27:49 +0600

    find the proper book details

--- src/page/BookDetails.tsx
+++ src/page/BookDetails.tsx
@@ -1,12 +1,18 @@
 import { useGetProductsQuery } from "@/redux/features/api/apiSlice";
-import React from "react";
+import { IProduct } from "@/types/globalTypes";
 import { useParams } from "react-router-dom";
 
 const BookDetails = () => {
   const { data } = useGetProductsQuery(undefined);
-  const product = useParams();
+  const productId = useParams();
+
+  const product = data?.find((item: IProduct) => {
+    if (item._id === productId.id) {
+      return item;
+    }
+  });
+
   console.log(product);
-  console.log(data);
   return <div></div>;
 };
 
