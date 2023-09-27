baf785cb03a375b77f19f6f3653aa6fefd6c1036
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-27 22:51:10 +0600

    added styles to the add book from

--- src/page/AddNewBook.tsx
+++ src/page/AddNewBook.tsx
@@ -6,14 +6,16 @@ const AddNewBook = () => {
   return (
     <div>
       <h1>Add A New Book</h1>
-      <section className="w-6/12 mx-auto">
+      <section className="w-6/12 mx-auto mt-5">
         <form>
-          <Input type="text" placeholder="Title" />
-          <Input type="text" placeholder="Genre" />
-          <Input type="text" placeholder="Price" />
-          <Input type="text" placeholder="Publication Date" />
-          <Textarea placeholder="Book Description" />
-          <Button variant="default">Add</Button>
+          <Input type="text" placeholder="Title" className="mb-2" />
+          <Input type="text" placeholder="Genre" className="mb-2" />
+          <Input type="text" placeholder="Price" className="mb-2" />
+          <Input type="text" placeholder="Publication Date" className="mb-2" />
+          <Textarea placeholder="Book Description" className="mb-2" />
+          <Button variant="default" className="w-full">
+            Add
+          </Button>
         </form>
       </section>
     </div>
