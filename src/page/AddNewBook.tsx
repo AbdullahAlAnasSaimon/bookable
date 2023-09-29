9327cfde0e1825cc042affab90847266d19d7579
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-29 23:48:26 +0600

    modifying the add new book input form

--- src/page/AddNewBook.tsx
+++ src/page/AddNewBook.tsx
@@ -5,11 +5,13 @@ import { Textarea } from "@/components/ui/textarea";
 const AddNewBook = () => {
   return (
     <div>
-      <h1>Add A New Book</h1>
+      <h1 className="text-center text-xl font-semibold">Add A New Book</h1>
       <section className="w-6/12 mx-auto mt-5">
         <form>
-          <Input type="text" placeholder="Title" className="mb-2" />
-          <Input type="text" placeholder="Genre" className="mb-2" />
+          <div>
+            <Input type="text" placeholder="Title" className="mb-2" />
+            <Input type="text" placeholder="Genre" className="mb-2" />
+          </div>
           <Input type="text" placeholder="Price" className="mb-2" />
           <Input type="text" placeholder="Publication Date" className="mb-2" />
           <Textarea placeholder="Book Description" className="mb-2" />
