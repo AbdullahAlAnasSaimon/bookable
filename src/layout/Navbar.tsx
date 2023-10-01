43bfebe129f598db95db19290b21d3a9705ffe94
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-10-01 21:04:50 +0600

    modifying the navbar after user authenticated

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -2,15 +2,53 @@ import { Link } from "react-router-dom";
 import { Button } from "../components/ui/button";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { setUser } from "@/redux/features/user/userSlice";
+/* import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu"; */
+import {
+  Cloud,
+  CreditCard,
+  Github,
+  Keyboard,
+  LifeBuoy,
+  LogOut,
+  Mail,
+  MessageSquare,
+  Plus,
+  PlusCircle,
+  Settings,
+  User,
+  UserPlus,
+  Users,
+} from "lucide-react";
+
+// import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
-import { ChevronDown, LogOut } from "lucide-react";
+} from "@/components/ui/dropdown-menu";
+// import { ChevronDown, LogOut } from "lucide-react";
 import { signOut } from "firebase/auth";
 import { auth } from "@/lib/firebase";
+// import { DropdownMenuShortcut } from "@/components/ui/dropdown-menu";
 
 export default function Navbar() {
   const { user } = useAppSelector((state) => state.user);
@@ -20,6 +58,22 @@ export default function Navbar() {
     signOut(auth);
     dispatch(setUser(null));
   };
+
+  /* <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <Button variant="default">
+                        {user?.email} {<ChevronDown className="ml-2 h-4 w-4" />}
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-30 mt-2">
+                      <DropdownMenuItem>
+                        <Button onClick={handleLogout} variant="default">
+                          <LogOut className="mr-2 h-4 w-4 inline-block" />
+                          Log out
+                        </Button>
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu> */
 
   return (
     <nav className="w-full h-12 backdrop-blur-lg z-10 bg-white-60 drop-shadow-lg">
@@ -41,17 +95,87 @@ export default function Navbar() {
               <li>
                 {user.email ? (
                   <DropdownMenu>
-                    <DropdownMenuTrigger>
-                      <Button variant="default">
-                        {user?.email} {<ChevronDown className="ml-2 h-4 w-4" />}
-                      </Button>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="link">{user?.email}</Button>
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-30 mt-2">
+                    <DropdownMenuContent className="w-56">
+                      <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuGroup>
+                        <DropdownMenuItem>
+                          <User className="mr-2 h-4 w-4" />
+                          <span>Profile</span>
+                          <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                          <CreditCard className="mr-2 h-4 w-4" />
+                          <span>Billing</span>
+                          <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                          <Settings className="mr-2 h-4 w-4" />
+                          <span>Settings</span>
+                          <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                          <Keyboard className="mr-2 h-4 w-4" />
+                          <span>Keyboard shortcuts</span>
+                          <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                      </DropdownMenuGroup>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuGroup>
+                        <DropdownMenuItem>
+                          <Users className="mr-2 h-4 w-4" />
+                          <span>Team</span>
+                        </DropdownMenuItem>
+                        <DropdownMenuSub>
+                          <DropdownMenuSubTrigger>
+                            <UserPlus className="mr-2 h-4 w-4" />
+                            <span>Invite users</span>
+                          </DropdownMenuSubTrigger>
+                          <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                              <DropdownMenuItem>
+                                <Mail className="mr-2 h-4 w-4" />
+                                <span>Email</span>
+                              </DropdownMenuItem>
+                              <DropdownMenuItem>
+                                <MessageSquare className="mr-2 h-4 w-4" />
+                                <span>Message</span>
+                              </DropdownMenuItem>
+                              <DropdownMenuSeparator />
+                              <DropdownMenuItem>
+                                <PlusCircle className="mr-2 h-4 w-4" />
+                                <span>More...</span>
+                              </DropdownMenuItem>
+                            </DropdownMenuSubContent>
+                          </DropdownMenuPortal>
+                        </DropdownMenuSub>
+                        <DropdownMenuItem>
+                          <Plus className="mr-2 h-4 w-4" />
+                          <span>New Team</span>
+                          <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                      </DropdownMenuGroup>
+                      <DropdownMenuSeparator />
                       <DropdownMenuItem>
-                        <Button onClick={handleLogout} variant="default">
-                          <LogOut className="mr-2 h-4 w-4 inline-block" />
-                          Log out
-                        </Button>
+                        <Github className="mr-2 h-4 w-4" />
+                        <span>GitHub</span>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <LifeBuoy className="mr-2 h-4 w-4" />
+                        <span>Support</span>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem disabled>
+                        <Cloud className="mr-2 h-4 w-4" />
+                        <span>API</span>
+                      </DropdownMenuItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem onClick={handleLogout}>
+                        <LogOut className="mr-2 h-4 w-4" />
+                        <span>Log out</span>
+                        <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
                       </DropdownMenuItem>
                     </DropdownMenuContent>
                   </DropdownMenu>
