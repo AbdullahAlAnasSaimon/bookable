e689067c1064e51155216473d2e178824f61bedb
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-10-24 23:07:34 +0600

    added conditionaly redering for finished reading books

--- src/page/CurrentlyReading.tsx
+++ src/page/CurrentlyReading.tsx
@@ -63,76 +63,79 @@ const CurrentlyReading = () => {
 
   return (
     <div className="w-10/12 mx-auto">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">S/N</TableHead>
-            <TableHead>Product Info</TableHead>
-            <TableHead>Author</TableHead>
-            <TableHead>Action</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {matchingProducts?.map((item: IProduct, index) => (
-            <TableRow key={item?._id}>
-              <TableCell className="font-medium">{index + 1}</TableCell>
-              <TableCell className="flex gap-5">
-                <div>
-                  <img src={item?.photo} alt="" className="w-10 h-16" />
-                </div>
-                <div>
-                  <p>{item?.title}</p>
-                  <p className="text-sm italic">{item?.genre}</p>
-                  <p className="font-semibold text-sm">$ {item?.price}</p>
-                </div>
-              </TableCell>
-              <TableCell>
-                <p>{item?.author}</p>
-              </TableCell>
-              <TableCell>
-                <div>
-                  <button className="text-[12px] px-3 py-1 rounded-full mr-2 bg-slate-900 text-white hover:bg-slate-700">
-                    Continue Reading
-                  </button>
-                  {matchingPairs?.finishedReading && (
-                    <p className="inline-block text-[12px] px-3 py-1 rounded-full mr-2 bg-green-500 text-white">
-                      Finished Reading
-                    </p>
-                  )}
-                  <Dialog>
-                    <DialogTrigger asChild>
-                      {!matchingPairs?.finishedReading && (
-                        <button className="text-[12px] px-3 py-1 rounded-full mr-2 bg-red-500 text-white hover:bg-red-600">
-                          Finish Reading
-                        </button>
-                      )}
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px]">
-                      <DialogHeader>
-                        <DialogTitle>
-                          Finished Reading? - {item?.title}
-                        </DialogTitle>
-                        <DialogDescription>
-                          Are you sure? you finished the "{item?.title}" book.
-                        </DialogDescription>
-                      </DialogHeader>
-                      <DialogFooter>
-                        <Button
-                          onClick={() => confirmFinishRading(item?._id)}
-                          type="submit"
-                          variant="destructive"
-                        >
-                          Confirm Finish
-                        </Button>
-                      </DialogFooter>
-                    </DialogContent>
-                  </Dialog>
-                </div>
-              </TableCell>
+      {matchingPairs?.finishedReading ? (
+        <p>Currently No Reading Book available!</p>
+      ) : (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">S/N</TableHead>
+              <TableHead>Product Info</TableHead>
+              <TableHead>Author</TableHead>
+              <TableHead>Action</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {matchingProducts?.map((item: IProduct, index) => (
+              <TableRow key={item?._id}>
+                <TableCell className="font-medium">{index + 1}</TableCell>
+                <TableCell className="flex gap-5">
+                  <div>
+                    <img src={item?.photo} alt="" className="w-10 h-16" />
+                  </div>
+                  <div>
+                    <p>{item?.title}</p>
+                    <p className="text-sm italic">{item?.genre}</p>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <p>{item?.author}</p>
+                </TableCell>
+                <TableCell>
+                  <div>
+                    <button className="text-[12px] px-3 py-1 rounded-full mr-2 bg-slate-900 text-white hover:bg-slate-700">
+                      Continue Reading
+                    </button>
+                    {matchingPairs?.finishedReading && (
+                      <p className="inline-block text-[12px] px-3 py-1 rounded-full mr-2 bg-green-500 text-white">
+                        Finished Reading
+                      </p>
+                    )}
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        {!matchingPairs?.finishedReading && (
+                          <button className="text-[12px] px-3 py-1 rounded-full mr-2 bg-red-500 text-white hover:bg-red-600">
+                            Finish Reading
+                          </button>
+                        )}
+                      </DialogTrigger>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                          <DialogTitle>
+                            Finished Reading? - {item?.title}
+                          </DialogTitle>
+                          <DialogDescription>
+                            Are you sure? you finished the "{item?.title}" book.
+                          </DialogDescription>
+                        </DialogHeader>
+                        <DialogFooter>
+                          <Button
+                            onClick={() => confirmFinishRading(item?._id)}
+                            type="submit"
+                            variant="destructive"
+                          >
+                            Confirm Finish
+                          </Button>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
     </div>
   );
 };
