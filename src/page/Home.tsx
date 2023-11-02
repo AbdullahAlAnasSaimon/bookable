d2ba2b97123d798200d1cefe7abf53a47abdb0e2
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-11-02 23:56:15 +0600

    added 5 book data to the home page

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <section className="w-11/12 mx-auto my-10">
       <div className="grid grid-cols-4 gap-5">
-        {data?.slice(0, 4)?.map((products: IProduct) => (
+        {data?.slice(0, 5)?.map((products: IProduct) => (
           <ProductsCard key={products?._id} product={products} />
         ))}
       </div>
